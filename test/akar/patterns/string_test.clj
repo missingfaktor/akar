10a15a48d17abc12ee454382a62fb4fd2d076c4c
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2015-10-24 12:45:51 +0200

    Add tests for string patterns

---
+++ test/akar/patterns/string_test.clj
@@ -0,0 +1,21 @@
+(ns akar.patterns.string-test
+  (:require [akar.patterns.string :refer :all]
+            [akar.patterns.basic :refer :all]
+            [akar.primitives :refer :all]
+            [akar.combinators :refer :all]
+            [clojure.test :refer :all]))
+
+(deftest string-combinators
+  (testing "!regex"
+    (let [block (clauses
+                  (!regex #"F (.*) (.*)") (fn [[handle name]]
+                                            {:event :followed
+                                             :handle handle
+                                             :name name})
+                  !any (fn [] :bad-event))]
+      (is (= {:event :followed
+              :handle "@doofus"
+              :name "Doofus"}
+             (try-match "F @doofus Doofus" block)))
+      (is (= :bad-event
+             (try-match "F X" block))))))
