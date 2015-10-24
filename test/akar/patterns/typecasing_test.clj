0f4a5f4fecdec81730012eed2b0db686858903dc
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2015-10-24 12:45:34 +0200

    Add tests for type-casing patterns

---
+++ test/akar/patterns/typecasing_test.clj
@@ -0,0 +1,32 @@
+(ns akar.patterns.typecasing-test
+  (:require [akar.patterns.typecasing :refer :all]
+            [akar.patterns.basic :refer :all]
+            [akar.primitives :refer :all]
+            [akar.combinators :refer :all]
+            [clojure.test :refer :all])
+  (:import [clojure.lang Keyword]))
+
+(deftest class-pattern
+
+  (testing "class match"
+    (let [block (clauses
+                  (!class String) (fn [] :string)
+                  (!class Keyword) (fn [] :keyword))]
+      (is (= :string
+             (try-match "SomeString" block)))
+      (is (= :keyword
+             (try-match :some-keyword block)))
+      (is (= nil
+             (try-match 4 block)))))
+
+  (testing "tag match"
+    (let [block (clauses
+                  (!tag :some-tag) (fn [] :yes))]
+      (is (= :yes
+             (try-match {:tag :some-tag} block)))))
+
+  (testing "tag match"
+    (let [block (clauses
+                  (!and (!type :card) !var) (fn [card] (:details card)))]
+      (is (= "Details"
+             (try-match (with-meta {:details "Details"} {:type :card}) block))))))
