9f444438805fd1af9d41f83b65dd35b879332d27
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2015-10-25 14:25:23 +0100

    Add !further-many combinator
    This is similar in spirit to Scala's `unapplySeq`.

--- src/akar/further.clj
+++ src/akar/further.clj
@@ -5,15 +5,23 @@
 ;
 ; Let's define a combinator that allows us to "further" patterns.
 
+(defn ^:private fan-out [root-extracts !further-patterns]
+  (let [pairings (map vector root-extracts !further-patterns)]
+    (reduce
+      (fn [extracts [in pattern]]
+        (let [new-extracts (pattern in)]
+          (if (nil? new-extracts)
+            (reduced nil)
+            (concat extracts new-extracts))))
+      []
+      pairings)))
+
 (defn !further [!root-pattern & !further-patterns]
   (fn [arg]
     (if-let [root-extracts (!root-pattern arg)]
-      (let [pairings (map vector root-extracts !further-patterns)]
-        (reduce
-          (fn [extracts [in pattern]]
-            (let [new-extracts (pattern in)]
-              (if (nil? new-extracts)
-                (reduced nil)
-                (concat extracts new-extracts))))
-          []
-          pairings)))))
+      (fan-out root-extracts !further-patterns))))
+
+(defn !further-many [!root-pattern !further-patterns]
+  (fn [arg]
+    (if-let [[root-extracts] (!root-pattern arg)]
+      (fan-out root-extracts !further-patterns))))
--- test/akar/further_test.clj
+++ test/akar/further_test.clj
@@ -2,16 +2,30 @@
   (:require [clojure.test :refer :all]
             [akar.patterns.basic :refer :all]
             [akar.patterns.collection :refer :all]
+            [akar.patterns.string :refer :all]
             [akar.further :refer :all]
             [akar.primitives :refer :all]))
 
 (deftest further-test
 
   (testing "!further"
+    (let [block (clauses
+                  (!further !cons !var !var) (fn [hd tl]
+                                               {:hd hd
+                                                :tl tl}))]
+      (testing "'furthers' a pattern"
+        (is (= {:hd 3 :tl [4 5]}
+               (match [3 4 5] block))))))
 
-    (testing "'furthers' patterns"
-      (is (= {:hd 3 :tl [4 5]}
-             (match [3 4 5] (clauses
-                              (!further !cons !var !var) (fn [hd tl]
-                                                           {:hd hd
-                                                            :tl tl}))))))))
+  (testing "!further-many"
+    (let [!address (!regex #"(.*) (.*), (.*)")
+          !berlin (!cst "Berlin")
+          block (clauses
+                  (!further-many !address [!var !var !berlin]) (fn [street house-nr]
+                                                                 {:street   street
+                                                                  :house-nr house-nr}))]
+      (testing "'furthers' a pattern into variadic patterns list"
+        (is (= {:street "Jahnstraße" :house-nr "21"}
+               (try-match "Jahnstraße 21, Berlin" block)))
+        (is (= clause-not-applied
+               (try-match "Jahnstraße 21, Hamburg" block)))))))
