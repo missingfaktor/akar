0af47b9fc0b1d88ec36488739a182b32c8a15842
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2015-10-26 10:13:12 +0100

    Fix a bug with !regex pattern
    When there are no captures, re-seq returns us a list of strings,
    instead of a list of list of strings. This needs special handling.

--- src/akar/patterns/string.clj
+++ src/akar/patterns/string.clj
@@ -3,5 +3,9 @@
 (defn !regex [rgx]
   (fn [arg]
     (if (string? arg)
-      (if-let [captures (re-seq rgx arg)]
-        [(->> captures first rest)]))))
+      (if-let [out (some->> arg
+                            (re-seq rgx)
+                            first)]
+        (cond
+          (string? out) []
+          (sequential? out) [(rest out)])))))
--- test/akar/patterns/string_test.clj
+++ test/akar/patterns/string_test.clj
@@ -9,17 +9,34 @@
 
   (testing "!regex"
     (let [block (clauses
-                  (!regex #"F (.*) (.*)") (fn [[handle name]]
-                                            {:event :followed
-                                             :handle handle
-                                             :name name})
+                  (!regex #"^F (.*) (.*)$") (fn [[handle name]]
+                                              {:event  :followed
+                                               :handle handle
+                                               :name   name})
                   !any (fn [] :bad-event))]
-      (is (= {:event :followed
-              :handle "@doofus"
-              :name "Doofus"}
-             (try-match "F @doofus Doofus" block)))
-      (is (= :bad-event
-             (try-match "F X" block)))
 
-      (is (= :bad-event
-             (try-match :not-even-a-string block))))))
+      (testing "captures values from a string that matches regex"
+        (is (= {:event  :followed
+                :handle "@doofus"
+                :name   "Doofus"}
+               (try-match "F @doofus Doofus" block))))
+
+      (testing "doesn't match invalid strings"
+        (is (= :bad-event
+               (try-match "F X" block))))
+
+      (testing "doesn't match non-strings"
+        (is (= :bad-event
+               (try-match :not-even-a-string block)))))
+
+    (let [block (clauses
+                  (!regex #"^F [0-9]{1}$") (fn [] :match)
+                  !any (fn [] :no-match))]
+
+      (testing "matches a string against a regex that captures nothing"
+        (is (= :match
+               (try-match "F 7" block))))
+
+      (testing "doesn't match invalid srings"
+        (is (= :no-match
+               (try-match "F 11" block)))))))
