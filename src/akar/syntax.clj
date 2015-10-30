bbb8878021cfaee760ad76395e96a0b704487b9a
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2015-10-30 19:51:17 +0100

    Change naming convention for seqex rules and syntaxes
    Earlier I thought I could just use PascalCase, but turns out it looks
    horrible in syndoc.

--- src/akar/syntax.clj
+++ src/akar/syntax.clj
@@ -4,52 +4,52 @@
             [akar.primitives :refer :all]
             [akar.patterns.basic :refer :all]))
 
-(sy/defterminal NumberLiteral number?)
-(sy/defterminal StringLiteral string?)
-(sy/defterminal BooleanLiteral (partial instance? Boolean))
-(sy/defterminal KeywordLiteral keyword?)
-(sy/defterminal NilLiteral nil?)
+(sy/defterminal number-literal number?)
+(sy/defterminal string-literal string?)
+(sy/defterminal boolean-literal (partial instance? Boolean))
+(sy/defterminal keyword-literal keyword?)
+(sy/defterminal nil-literal nil?)
 
-(sy/defrule Literal
-            (cap (sy/alt NumberLiteral
-                         StringLiteral
-                         BooleanLiteral
-                         KeywordLiteral
-                         NilLiteral)
+(sy/defrule literal
+            (cap (sy/alt number-literal
+                         string-literal
+                         boolean-literal
+                         keyword-literal
+                         nil-literal)
                  (fn [[lit]]
                    `(!cst ~lit))))
 
-(sy/defrule Any
+(sy/defrule any-rule
             (cap (sy/alt :_ :any)
                  (constantly `!any)))
 
-(sy/defrule Pattern
-            (sy/alt Any
-                    Literal))
+(sy/defrule pattern-rule
+            (sy/alt any-rule
+                    literal))
 
-(sy/defrule Clause
-            (recap (sy/cat Pattern (cap sy/form))
+(sy/defrule clause-rule
+            (recap (sy/cat pattern-rule (cap sy/form))
                    (fn [pat [action]]
                      `(clause* ~pat (fn []
                                       ~action)))))
 
-(sy/defrule Clauses
-            (recap (sy/rep+ Clause)
+(sy/defrule clauses-rule
+            (recap (sy/rep+ clause-rule)
                    (fn [& clss]
                      `(or-else ~@clss))))
 
-(sy/defrule Match
-            (recap (sy/cat (cap sy/form) Clauses)
+(sy/defrule match-rule
+            (recap (sy/cat (cap sy/form) clauses-rule)
                    (fn [[arg] clss]
                      `(match* ~arg ~clss))))
 
-(sy/defrule TryMatch
-            (recap (sy/cat (cap sy/form) Clauses)
+(sy/defrule try-match-rule
+            (recap (sy/cat (cap sy/form) clauses-rule)
                    (fn [[arg] clss]
                      `(try-match* ~arg ~clss))))
 
 ; Macros
-(sy/defsyntax clause Clause)
-(sy/defsyntax clauses Clauses)
-(sy/defsyntax match Match)
-(sy/defsyntax try-match TryMatch)
+(sy/defsyntax clause clause-rule)
+(sy/defsyntax clauses clauses-rule)
+(sy/defsyntax match match-rule)
+(sy/defsyntax try-match try-match-rule)
