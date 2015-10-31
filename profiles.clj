0d94d8ffa59eb91c519e52c2e41d82176f21c96d
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2015-10-31 10:43:34 +0100

    Add clj-stacktrace plugin
    To turn shitty Clojure stack traces into something a tad less shitty.

--- profiles.clj
+++ profiles.clj
@@ -1,1 +1,7 @@
-{:user {:plugins [[jonase/eastwood "0.2.1"]]}}
+{:user {:plugins      [[jonase/eastwood "0.2.1"]]
+        :dependencies [[clj-stacktrace "0.2.8"]]
+        :injections   [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
+                                    'print-cause-trace)
+                             new (ns-resolve (doto 'clj-stacktrace.repl require)
+                                   'pst)]
+                         (alter-var-root orig (constantly (deref new))))]}}
