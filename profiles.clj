34e40fdb21109399cc7f495a12fb7a79435db910
Aleksander Sumowski <sumek@o2.pl>
2015-11-04 20:19:33 +0000

    :alien:cleanup project.clj

--- profiles.clj
+++
@@ -1,7 +0,0 @@
-{:user {:plugins      [[jonase/eastwood "0.2.1"]]
-        :dependencies [[clj-stacktrace "0.2.8"]]
-        :injections   [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
-                                    'print-cause-trace)
-                             new (ns-resolve (doto 'clj-stacktrace.repl require)
-                                   'pst)]
-                         (alter-var-root orig (constantly (deref new))))]}}
