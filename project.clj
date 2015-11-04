6228a7fb7fde638d166fb2acfd2e1ae8cb3fcf93
Aleksander Sumowski <sumek@o2.pl>
2015-11-04 20:43:41 +0000

    alias for running eastwood before tests

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [n01se/seqex "2.0.1"]]
-  :profiles {:dev {:plugins [[jonase/eastwood "0.2.1"]]}})
+  :profiles {:dev {:plugins [[jonase/eastwood "0.2.1"]]}}
+  :aliases  {"et" ["do" ["eastwood"] ["test"]]})
