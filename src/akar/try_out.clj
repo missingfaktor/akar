a09c535e6df1f053be205930e31e735c4345d304
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2016-07-03 22:13:26 +0100

    Expand try-out namespace to include more namespaces

--- src/akar/try_out.clj
+++ src/akar/try_out.clj
@@ -1,3 +1,7 @@
 (ns akar.try-out
+  (:require [clojure.repl :refer :all])
+  (:require [akar.primitives :refer :all])
+  (:require [akar.patterns :refer :all])
+  (:require [akar.combinators :refer :all])
   (:require [akar.syntax :refer :all])
   (:require [n01se.syntax :refer :all]))
