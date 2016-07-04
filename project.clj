842fd00bf71efcd7e04fa6ec16da8170ce7d1441
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2016-07-05 00:53:13 +0100

    Sign with a GPG key

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :license {:name         "The MIT License"
             :url          "http://opensource.org/licenses/mit-license.php"
             :distribution :repo}
+  :signing {:gpg-key "F5EEC7BF"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [n01se/seqex "2.0.1"]]
   :profiles {:dev {:plugins [[jonase/eastwood "0.2.3"]]}}
