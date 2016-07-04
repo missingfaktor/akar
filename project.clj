80113a7e61d6db2842b83129d609ed0cce2ec72a
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2016-07-05 00:32:16 +0100

    Upgrade Eastwood to 0.2.3

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [n01se/seqex "2.0.1"]]
-  :profiles {:dev {:plugins [[jonase/eastwood "0.2.1"]]}}
-  :aliases  {"et" ["do" ["eastwood"] ["test"]]}
+  :profiles {:dev {:plugins [[jonase/eastwood "0.2.3"]]}}
+  :aliases {"et" ["do" ["eastwood"] ["test"]]}
   :main akar.try-out)
