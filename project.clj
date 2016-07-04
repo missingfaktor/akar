c1ddb934bd5fb3cfd20b9cd3ea4e5ab16629b3cd
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2016-07-05 00:33:26 +0100

    Fix license in the project file

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject akar "0.1.0-SNAPSHOT"
   :description "First-class patterns for Clojure"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name         "The MIT License"
+            :url          "http://opensource.org/licenses/mit-license.php"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [n01se/seqex "2.0.1"]]
   :profiles {:dev {:plugins [[jonase/eastwood "0.2.3"]]}}
