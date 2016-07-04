f4eb38b6162533a77dbe9f449f7ad804dcd9987d
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2016-07-05 00:33:03 +0100

    Upgrade Clojure to 1.8.0

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "First-class patterns for Clojure"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [n01se/seqex "2.0.1"]]
   :profiles {:dev {:plugins [[jonase/eastwood "0.2.3"]]}}
   :aliases {"et" ["do" ["eastwood"] ["test"]]}
