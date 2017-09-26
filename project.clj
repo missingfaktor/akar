c004444414e3a6d5ff5c9fd5a3fdd1d5b7bcb289
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2017-09-26 12:48:07 +0100

    Update license in Leiningen project file

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject akar "0.1.0"
   :description "First-class patterns for Clojure"
-  :license {:name         "The MIT License"
-            :url          "http://opensource.org/licenses/mit-license.php"
+  :license {:name         "Apache License 2.0"
+            :url          "http://www.apache.org/licenses/LICENSE-2.0"
             :distribution :repo}
   :signing {:gpg-key "F5EEC7BF"}
   :dependencies [[org.clojure/clojure "1.8.0"]
