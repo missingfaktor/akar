94de06083886cc5017dbfabb25af65b72bad1ce2
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2017-11-06 23:15:55 +0000

    Disable release signing
    This finally allowed me to simply deploy with `lein deploy clojars`.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name         "Apache License 2.0"
             :url          "http://www.apache.org/licenses/LICENSE-2.0"
             :distribution :repo}
-  :signing {:gpg-key "F5EEC7BF"}
+  :repositories [["clojars" {:url "https://clojars.org/repo"
+                             :sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [n01se/seqex "2.0.1"]]
   :profiles {:dev {:plugins [[jonase/eastwood "0.2.3"]]}}
