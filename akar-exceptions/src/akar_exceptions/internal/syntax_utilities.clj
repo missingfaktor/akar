373516083f3434f55ddfbde2158f13e3f423a30f
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2018-04-04 00:31:15 +0100

    Use more traditional names in syntax definition utilities

--- akar-exceptions/src/akar_exceptions/internal/syntax_utilities.clj
+++ akar-exceptions/src/akar_exceptions/internal/syntax_utilities.clj
@@ -1,15 +1,15 @@
 (ns akar-exceptions.internal.syntax-utilities
   (:require [clojure.spec.alpha :as sp]))
 
-(defn define-syntax* [name' forms spec target]
+(defn define-syntax* [name' forms spec codegen]
   (let [enclosing-form (cons name' forms)
-        result (sp/conform spec enclosing-form)]
-    (if (= ::sp/invalid result)
+        syntax-tree (sp/conform spec enclosing-form)]
+    (if (= ::sp/invalid syntax-tree)
       (throw (ex-info "Syntax error" (sp/explain-data spec enclosing-form)))
-      (target result))))
+      (codegen syntax-tree))))
 
-(defmacro define-syntax [name & {:keys [spec-name spec target]}]
+(defmacro define-syntax [name & {:keys [parser codegen]}]
   `(do
-     (sp/def ~spec-name ~spec)
+     (sp/def ~(:name parser) ~(:spec parser))
      (defmacro ~name [& forms#]
-       (define-syntax* '~name forms# ~spec-name ~target))))
+       (define-syntax* '~name forms# ~(:name parser) ~codegen))))
