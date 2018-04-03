356d3ec3eb470b8fcb9a51ed5303a797c1ea3be4
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2018-04-03 23:07:45 +0100

    Attempt fixing `project.clj` files

--- akar-exceptions/project.clj
+++ akar-exceptions/project.clj
@@ -1,12 +1,13 @@
-(defproject akar/akar-exceptions "1.0.0"
+(defproject akar/akar-exceptions "0.0.1"
   :description "First-class patterns for Clojure – Exception handling"
   :url "www.github.com/missingfaktor/akar"
   :license {:name         "Apache License 2.0"
             :url          "http://www.apache.org/licenses/LICENSE-2.0"
             :distribution :repo}
-  :repositories [["clojars" {:url           "https://clojars.org/akar/akar-exceptions"
-                             :sign-releases false}]]
+  :repositories [["akar-core-clojars" {:url           "https://clojars.org/akar-core"
+                                       :sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [n01se/seqex "2.0.1"]]
+                 [n01se/seqex "2.0.1"]
+                 [akar/akar-core "1.0.0"]]
   :profiles {:dev {:plugins [[jonase/eastwood "0.2.3"]]}}
   :aliases {"et" ["do" ["eastwood"] ["test"]]})
--- project.clj
+++ project.clj
@@ -4,10 +4,9 @@
   :license {:name         "Apache License 2.0"
             :url          "http://www.apache.org/licenses/LICENSE-2.0"
             :distribution :repo}
-  :repositories [["clojars" {:url           "https://clojars.org/akar"
-                             :sign-releases false}]]
+  :repositories [["akar-core-clojars" {:url           "https://clojars.org/akar/akar-core"
+                                       :sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [akar/akar-core "1.0.0"]]
   :plugins [[lein-sub "0.3.0"]]
-  :sub ["akar-core"
-        "akar-exceptions"])
+  :sub ["akar-core"])
