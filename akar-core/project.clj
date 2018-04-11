d50d7e198f5007eb9902f6a226752342b50a9a30
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2018-04-11 21:51:28 +0100

    Release v2.0.0 of akar-core
    * You now need `lein deploy clojars`.
    * :repositories has now become :deploy-repositories.
    * Clojars slug *has* to be `/repo`.

--- akar-core/project.clj
+++ akar-core/project.clj
@@ -1,13 +1,13 @@
-(defproject akar/akar-core "1.0.0"
-  :description "First-class patterns for Clojure – Core framework"
-  :url "www.github.com/missingfaktor/akar"
-  :license {:name         "Apache License 2.0"
-            :url          "http://www.apache.org/licenses/LICENSE-2.0"
-            :distribution :repo}
-  :repositories [["clojars" {:url           "https://clojars.org/akar/akar-core"
-                             :sign-releases false}]]
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [n01se/seqex "2.0.1"]]
-  :profiles {:dev {:plugins [[jonase/eastwood "0.2.3"]]}}
-  :aliases {"et" ["do" ["eastwood"] ["test"]]}
-  :main akar.try-out)
+(defproject akar/akar-core "2.0.0"
+            :description "First-class patterns for Clojure – Core framework"
+            :url "www.github.com/missingfaktor/akar"
+            :license {:name         "Apache License 2.0"
+                      :url          "http://www.apache.org/licenses/LICENSE-2.0"
+                      :distribution :repo}
+            :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
+                                              :sign-releases false}]]
+            :dependencies [[org.clojure/clojure "1.9.0"]
+                           [n01se/seqex "2.0.1"]]
+            :profiles {:dev {:plugins [[jonase/eastwood "0.2.3"]]}}
+            :aliases {"et" ["do" ["eastwood"] ["test"]]}
+            :main akar.try-out)
