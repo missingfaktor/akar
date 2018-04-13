3561861bc49f87cde5855ad2917add3e605cf181
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2018-04-13 01:18:05 +0100

    Make akar (umbrella project) v2.0.0 release

--- project.clj
+++ project.clj
@@ -1,12 +1,14 @@
-(defproject akar "1.0.0"
+(defproject akar "2.0.0"
   :description "First-class patterns for Clojure"
   :url "www.github.com/missingfaktor/akar"
   :license {:name         "Apache License 2.0"
             :url          "http://www.apache.org/licenses/LICENSE-2.0"
             :distribution :repo}
-  :repositories [["akar-core-clojars" {:url           "https://clojars.org/akar/akar-core"
-                                       :sign-releases false}]]
+  :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
+                                    :sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [akar/akar-core "1.0.0"]]
+                 [akar/akar-core "2.0.1"]
+                 [akar/akar-exceptions "0.0.2"]
+                 [akar/akar-commons "0.0.1"]]
   :plugins [[lein-sub "0.3.0"]]
   :sub ["akar-core"])
