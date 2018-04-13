d88a1ddab7bf5910796f01618548a714efc9f3fc
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2018-04-13 01:02:49 +0100

    Make akar-exceptions 0.0.2 release

--- akar-exceptions/project.clj
+++ akar-exceptions/project.clj
@@ -1,4 +1,4 @@
-(defproject akar/akar-exceptions "0.0.1"
+(defproject akar/akar-exceptions "0.0.2"
             :description "First-class patterns for Clojure – Exception handling"
             :url "www.github.com/missingfaktor/akar"
             :license {:name         "Apache License 2.0"
@@ -8,6 +8,6 @@
                                               :sign-releases false}]]
             :dependencies [[org.clojure/clojure "1.9.0"]
                            [n01se/seqex "2.0.2"]
-                           [akar/akar-core "2.0.0"]
+                           [akar/akar-core "2.0.1"]
                            [akar/akar-commons "0.0.1"]]
             :pedantic? :abort)
