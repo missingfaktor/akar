e9bc179ded1725caa744d93a2acb3767bb88a80a
missingfaktor <rahul.phulore.999@gmail.com>
2024-07-28 20:26:11 +0100

    Upgrade `akar-commons` to Clojure 1.11.3

--- akar-commons/project.clj
+++ akar-commons/project.clj
@@ -1,9 +1,9 @@
-(defproject akar/akar-commons "0.0.1"
-            :description "A bag of common utility functions used by Akar projects"
-            :url "www.github.com/missingfaktor/akar"
-            :license {:name         "Apache License 2.0"
-                      :url          "http://www.apache.org/licenses/LICENSE-2.0"
-                      :distribution :repo}
-            :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
-                                              :sign-releases false}]]
-            :dependencies [[org.clojure/clojure "1.9.0"]])
+(defproject akar/akar-commons "1.0.0"
+  :description "A bag of common utility functions used by Akar projects"
+  :url "www.github.com/missingfaktor/akar"
+  :license {:name         "Apache License 2.0"
+            :url          "http://www.apache.org/licenses/LICENSE-2.0"
+            :distribution :repo}
+  :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
+                                    :sign-releases false}]]
+  :dependencies [[org.clojure/clojure "1.11.3"]])
