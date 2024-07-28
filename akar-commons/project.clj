13fe334cda7fc0ed228f3a6dbbd943bc99b0661f
missingfaktor <rahul.phulore.999@gmail.com>
2024-07-28 20:45:10 +0100

    Release `akar-commons:3.0.0`
    I created a new token under https://clojars.org/tokens.
    
    I created a file `~/.lein/credentials.clj` with the following
    contents:
    
    ```edn
    {#"https://repo.clojars.org"
     {:username "your-clojars-username"
      :password "your-clojars-deploy-token"}}
    ```
    
    Then I ran the following:
    
    ```shell
    gpg --default-recipient-self -e ~/.lein/credentials.clj > ~/.lein/credentials.clj.gpg
    ```
    
    Then I ran the following from the project root:
    
    ```shell
    lein deploy clojars
    ```

--- akar-commons/project.clj
+++ akar-commons/project.clj
@@ -1,9 +1,8 @@
-(defproject akar/akar-commons "1.0.0"
+(defproject akar/akar-commons "3.0.0"
   :description "A bag of common utility functions used by Akar projects"
   :url "www.github.com/missingfaktor/akar"
   :license {:name         "Apache License 2.0"
             :url          "http://www.apache.org/licenses/LICENSE-2.0"
             :distribution :repo}
-  :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
-                                    :sign-releases false}]]
+  :deploy-repositories [["clojars" :clojars]]
   :dependencies [[org.clojure/clojure "1.11.3"]])
