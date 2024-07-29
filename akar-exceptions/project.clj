eb5d0357dab89e76a7a7f2b8f6d5007b9324ab8e
missingfaktor <rahul.phulore.999@gmail.com>
2024-07-29 22:06:44 +0100

    Upgrade `akar-exceptions` to Clojure 1.11.3
    Also release version `3.0.0`.

--- akar-exceptions/project.clj
+++ akar-exceptions/project.clj
@@ -1,13 +1,11 @@
-(defproject akar/akar-exceptions "0.0.2"
-            :description "First-class patterns for Clojure – Exception handling"
-            :url "www.github.com/missingfaktor/akar"
-            :license {:name         "Apache License 2.0"
-                      :url          "http://www.apache.org/licenses/LICENSE-2.0"
-                      :distribution :repo}
-            :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
-                                              :sign-releases false}]]
-            :dependencies [[org.clojure/clojure "1.9.0"]
-                           [n01se/seqex "2.0.2"]
-                           [akar/akar-core "2.0.1"]
-                           [akar/akar-commons "0.0.1"]]
-            :pedantic? :abort)
+(defproject akar/akar-exceptions "3.0.0"
+  :description "First-class patterns for Clojure – Exception handling"
+  :url "www.github.com/missingfaktor/akar"
+  :license {:name         "Apache License 2.0"
+            :url          "http://www.apache.org/licenses/LICENSE-2.0"
+            :distribution :repo}
+  :deploy-repositories [["clojars" :clojars]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [n01se/seqex "2.0.2"]
+                 [akar/akar-core "3.0.0"]
+                 [akar/akar-commons "3.0.0"]])
