71bc5b77a9b0294f2c11c84e31098e4eebea380e
missingfaktor <rahul.phulore.999@gmail.com>
2024-07-29 22:05:00 +0100

    Upgrade `akar-core` to Clojure 1.11.3
    Also release version `3.0.0`.

--- akar-core/project.clj
+++ akar-core/project.clj
@@ -1,13 +1,12 @@
-(defproject akar/akar-core "2.0.1"
-            :description "First-class patterns for Clojure – Core framework"
-            :url "www.github.com/missingfaktor/akar"
-            :license {:name         "Apache License 2.0"
-                      :url          "http://www.apache.org/licenses/LICENSE-2.0"
-                      :distribution :repo}
-            :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
-                                              :sign-releases false}]]
-            :dependencies [[org.clojure/clojure "1.9.0"]
-                           [n01se/seqex "2.0.2"]
-                           [akar/akar-commons "0.0.1"]]
-            :pedantic? :abort
-            :main akar.try-out)
+(defproject akar/akar-core "3.0.0"
+  :description "First-class patterns for Clojure – Core framework"
+  :url "www.github.com/missingfaktor/akar"
+  :license {:name         "Apache License 2.0"
+            :url          "http://www.apache.org/licenses/LICENSE-2.0"
+            :distribution :repo}
+  :deploy-repositories [["clojars" :clojars]]
+  :dependencies [[org.clojure/clojure "1.11.3"]
+                 [n01se/seqex "2.0.2"]
+                 [akar/akar-commons "3.0.0"]]
+  :pedantic? :abort
+  :main akar.try-out)
